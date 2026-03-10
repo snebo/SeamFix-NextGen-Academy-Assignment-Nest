5ec97042e85634c62a75453bba4f494301755ee4
stefano N <30480337+snebo@users.noreply.github.com>
2026-03-10 19:00:17 +0100

    create auth module

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useLogger(Logger);
-  await app.listen(process.env.PORT ?? 3000);
 
-  const logger = new Logger('Bootstrap');
-  logger.log('hello');
-  logger.log(`app is running on ${await app.getUrl()}`);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
+
+  Logger.log(`App is running on: ${await app.getUrl()}`, 'Bootstrap');
 }
 bootstrap();
